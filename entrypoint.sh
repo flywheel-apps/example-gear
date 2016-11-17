36ea26d57324fbac54a828f57e782c723521d6ca
Nathaniel Kofalt <nathaniel@kofalt.com>
2016-11-17 15:46:07 -0600

    Add some flavor to the output!

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,23 @@
   # Set cwd
   unset CDPATH
   cd "$( dirname "${BASH_SOURCE[0]}" )"
+  
+  dest="/flywheel/v0/output/result.txt"
 
-  ls -1 "/flywheel/v0/input" > "/flywheel/v0/output/file_list.txt"
+  # Create a sample output file with some information.
+  echo "Welcome to the example gear!" > $dest
+  echo >> $dest
+  echo "Your configuration:" >> $dest
+  cat /flywheel/v0/config.json >> $dest
+  echo >> $dest
+  echo "Your chosne files:" >> $dest
+  ls -1 "/flywheel/v0/input" >> $dest
+  echo >> $dest
+  echo "The end!" >> $dest
+  
+  # Print out the results for demonstration purposes.
+  cat $dest
 
   # .metadata.json required to indicate gear completed successfully.
-  echo '{ "acquisition" : { "files" : [{ "name" : "file_list.txt" }] } }' > "/flywheel/v0/output/.metadata.json"
+  echo '{ "acquisition" : { "files" : [{ "name" : "result.txt" }] } }' > "/flywheel/v0/output/.metadata.json"
 )
