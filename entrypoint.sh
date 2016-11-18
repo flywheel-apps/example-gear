35b199353a6b2fedcec4094398e805af93488ca6
Nathaniel Kofalt <nathaniel@kofalt.com>
2016-11-18 13:13:27 -0600

    Update for clarity when using via CLI

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 (
-  #
-  # create result.txt with some example information.
-  #
+	#
+	# create result.txt with some example information.
+	#
 
-  # Set cwd
-  unset CDPATH
-  cd "$( dirname "${BASH_SOURCE[0]}" )"
-  
-  dest="/flywheel/v0/output/result.txt"
+	# Set cwd
+	unset CDPATH
+	cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-  # Create a sample output file with some information.
-  echo "Welcome to the example gear!" > $dest
-  echo >> $dest
-  echo "Your configuration:" >> $dest
-  cat /flywheel/v0/config.json >> $dest
-  echo >> $dest
-  echo "Your chosne files:" >> $dest
-  ls -1 "/flywheel/v0/input" >> $dest
-  echo >> $dest
-  echo "The end!" >> $dest
-  
-  # Print out the results for demonstration purposes.
-  cat $dest
+	dest="/flywheel/v0/output/result.txt"
 
-  # .metadata.json required to indicate gear completed successfully.
-  echo '{ "acquisition" : { "files" : [{ "name" : "result.txt" }] } }' > "/flywheel/v0/output/.metadata.json"
+	# Create a sample output file with some information.
+	echo "Welcome to the example gear!" > $dest
+	echo >> $dest
+	echo "Your configuration:" >> $dest
+	cat /flywheel/v0/config.json >> $dest
+	echo >> $dest
+	echo >> $dest
+	echo "Your input files:" >> $dest
+	find /flywheel/v0/input -type f >> $dest
+	echo >> $dest
+	echo "The end!" >> $dest
+
+	# Print out the results for demonstration purposes.
+	cat $dest
+
+	# .metadata.json required to indicate gear completed successfully.
+	echo '{ "acquisition" : { "files" : [{ "name" : "result.txt" }] } }' > "/flywheel/v0/output/.metadata.json"
 )
